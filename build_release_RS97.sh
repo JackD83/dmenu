350a694ef5d14a64745f4dcb12cdc321b65bf3be
mai <sebastian.maier@iosb.fraunhofer.de>
2018-01-26 11:17:18 +0100

    created rs97 build script added sdlfix.so added backup script

---
+++ build_release_RS97.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+make -f Makefile.dingoo RS97=1
+cd volume 
+make -f Makefile.dingux
+cd ..
+cd viewimage
+make -f Makefile.dingux
+cd ..
+
+mkdir -p build_release/dmenu
+cp -a installer build_release/dmenu
+cp -a resources build_release/dmenu
+cp -a scripts build_release/dmenu
+cp -a themes_RS97 build_release/dmenu/themes
+cp -a wallpapers build_release/dmenu
+
+mkdir -p build_release/dmenu/usb
+cp viewimage/usb.png build_release/dmenu/usb
+cp viewimage/viewimage.dge build_release/dmenu/usb
+
+mkdir -p build_release/dmenu/volume
+cp volume/setVolume build_release/dmenu/volume
+
+cp dmenu_RS97 build_release/dmenu/dmenu
+cp dmenuc.bin build_release/dmenu
+cp sdlfix.so build_release/dmenu
+
+cp dmenu.ini build_release/dmenu
+cp SwitchMenu.dge build_release/dmenu
